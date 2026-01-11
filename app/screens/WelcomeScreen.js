fc094f8d19670faef4dd36d0ad7aa01d7c9dc209
Skoco Pavle <skocopavle@gmail.com>
2026-01-11 21:42:24 +0100

    Final details

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -2,13 +2,15 @@ import {StyleSheet, View, SafeAreaView, Pressable, Image} from 'react-native';
 
 export default function Home() {
   return (
-    <>
-      <SafeAreaView style={styles.buttonContainer}>
-        <Pressable style={styles.button}>
-          <Image source={require('../assets/cig.jpg')} style={styles.image}/>
-        </Pressable>
-      </SafeAreaView>
-    </>
+    <View style={styles.container}>
+        <View style={styles.counter}>
+        </View>
+        <SafeAreaView style={styles.buttonContainer}>
+            <Pressable style={styles.button}>
+            <Image source={require('../assets/cig.jpg')} style={styles.image}/>
+            </Pressable>
+        </SafeAreaView>
+    </View>
   );
 }
 
@@ -16,7 +18,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flex: 1,
     justifyContent: 'flex-end',
-    paddingBottom: 90,
+    paddingBottom: 120,
     alignItems: 'center',
   },
 
@@ -24,6 +26,22 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
     borderRadius: 150,
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+  },
+
+  counter: {
+    backgroundColor: '#fff',
+    width: 370,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 450,
+    top: 80,
+    left: 20,
+    borderRadius: 10
   },
 
   image: {
