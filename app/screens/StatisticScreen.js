d1a8168dbbe58866f799e0dad1b185095bdafe0c
Skoco Pavle <skocopavle@gmail.com>
2026-01-24 14:30:41 +0100

    Added chart

--- app/screens/StatisticScreen.js
+++ app/screens/StatisticScreen.js
@@ -1,7 +1,10 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Pressable} from 'react-native';
 import { BarChart } from 'react-native-gifted-charts';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
+import { Color } from './constants/TWPalete';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 const colorThemes = {
   blue: { name: "blue", primary: 500, accent: 600 },
@@ -14,11 +17,62 @@ const colorThemes = {
 
 function StatisticScreen({navigation}) {
     const [colorTheme, setColorTheme] = useState("blue");
+    const theme = colorThemes[colorTheme];
+
+    const themeColor = Color[theme.name];
+
+    const bgColors = [
+        Color[theme.name][100],
+        "#ffffff",
+        Color[theme.name][100],
+    ];
+
+    const data = [{ value: 50}, {value: 80}, {value: 90}, {value: 70}];
+
     return (
-        <LinearGradient colors={["#000000", "#ffffff"]}>
-            <BarChart />
+        <LinearGradient colors={bgColors} style={{flex: 1}}>
+            <SafeAreaView style={{flex: 1}}>
+                <ScrollView contentInsetAdjustmentBehavior="automatic" style={{paddingTop: 20}}>
+                    <BarChart data={data}/>
+
+                    {/* Color theme selector*/}
+                    <View style={{paddingHorizontal: 16}}>
+                        <Text style={styles.subtitle}>Choose Theme</Text>
+
+                        <View style={{flexDirection: "row", gap: 16}}>
+                            {Object.keys(colorThemes).map((theme) => (
+                                <Pressable key={theme}>
+                                    <Text style={styles.label}>{theme}</Text>
+                                </Pressable>
+                            ))}
+                        </View>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         </LinearGradient>
     );
 }
 
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#ffffff",
+    borderRadius: 16,
+    padding: 16,
+    boxShadow: "0px 2px 8px rgba(0,0,0,0.05)",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "700",
+    color: Color.gray[900],
+  },
+  subtitle: {
+    fontSize: 16,
+    color: Color.gray[600],
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 14,
+    color: Color.gray[600],
+  },
+});
 export default StatisticScreen;
