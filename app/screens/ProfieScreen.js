56f22f0b15379d4342871ef553ce967aa439a68b
Skoco Pavle <skocopavle@gmail.com>
2026-03-20 21:47:29 +0100

    Commit

--- app/screens/ProfieScreen.js
+++ app/screens/ProfieScreen.js
@@ -5,7 +5,6 @@ function ProfieScreen() {
     const { themeColor, theme } = useTheme();
     return (
         <View style={{backgroundColor: themeColor[100], flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{top: +150, left: 120}} onPress={() => navigation.navigate('Home')}>Opa</Text>
         </View>
     )}
 export default ProfieScreen;
